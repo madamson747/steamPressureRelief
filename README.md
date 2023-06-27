95b9f3bf7d4a4be6a4a247cca6a334d45283edb5
madamson7 <bladehawk_123@hotmail.com>
2023-06-27 15:33:23 -0600

    added updated readme, the lua code and some scrrenshots and a diagram to get started

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# steamPressureRelief
-A collection of documents and lua code to automate the handling of excess steam production in modular reactors (Refined Power mod) for Satisfactory.
+# Steam Pressure Relief Automation
+This is a collection of documents and the lua code used to automate the handling of excess steam production in modular reactors[^1] for Satisfactory.
+The goal of this project was to enable me to go work on my other factories with less down time on the reactors due to over-producing steam (which triggers an auto-shutdown event).
+
+It is my hope / goal that this will help provide others interested or already experimenting with modular reactors and compressed steam another source for starting / improving their own setup.
+
+If you have any questions / comments please let me know.
+
+
+## How to use this repo / the code in your own factory
+For just a basic checklist follow the steps below, for more instructions i'm planning on making a wiki to go into more detail.
+- [ ] Build your factory for the Reactor Coolant and Advanced Reactor Core production
+- [ ] Build your reactors
+- [ ] Build the turbines and generators
+- [ ] Build the computer network
+- [ ] Copy / Paste in the lua code & run it
+## Factory Layout
+Here is a basic diagram of the layout of the factory (minus the portion to create the coolant and reactor cores).
+Yellow is the Ficsit Networks Network Cable, Blue is Compressed Steam.
+
+![Factory Diagram](/Diagrams/Factory%20Diagram.png)
+
+## To-Do List
+- [ ] Refactor the Lua code
+  - Need to make it cleaner and easier to read
+- [ ] Add a timer function for opening and closing the valve (so it will close after 'x' seconds)
+
+## Ideas for the future
+### Monitoring the steam
+I'm considering changing the way I monitor the steam production and amounts. I started this way because I adapted a version of this from what I had previously setup with the Circuitry mod (formly called Wiremod).[^2] That version would simply flush the small fluid buffers immediately "downstream" of the reactors so the reactor outputs wouldn't get backed up. While this worked I wanted to still use the steam that was produced.
+
+### Misc.
+- add graphs and charts on data screens
+  - production line graphs (steam, power, etc.)
+- add to / improve on the control panel
+- add networking components for remote monitoring at another location
+- create a shareable blueprint to put on [Satisfactory Calculator](https://satisfactory-calculator.com/)
+- eventually add structure / design to the factory so it doesn't just float above the lake
+
+## Mod List
+- [Ficsit Mod Loader](https://ficsit.app/sml-versions)
+  - uhh cuz...
+- [Refined Power](https://ficsit.app/mod/RefinedPower)
+  - Modular Reactors and necessary buildings / resources come from this mod.
+- [Ficsit Networks](https://ficsit.app/mod/FicsItNetworks)
+  - The computer that runs the Lua code and all associated components to connect your factory are from this mod.
+- I use plenty of others but those are the required mods for this project.
+
+
+## Screenshots
+
+__Factory__
+![](/Screenshots/Factory%20Overview.jpg)
+
+__Computer & Screens__
+![](/Screenshots/FIN%20Computer.jpg)
+
+__Reactors__
+![](Screenshots/Reactors.jpg)
+
+__Generators__
+![](/Screenshots/Generators.jpg)
+
+__Steam Buffers & Valves__
+![](/Screenshots/Steam%20Buffers%20and%20Valves.jpg)
+
+__Control Panel__
+![](/Screenshots/Control%20Panel%20Confirm%20Manual%20Flush.jpg)
+
+__Control Panel Ready for manually flushing the buffers__
+![](/Screenshots/Control%20Panel%20Ready%20to%20Manually%20Flush.jpg)
+
+
+__Control Panel after flushing the buffers__
+
+This looks similar to the previous but the LED displays in the bottom left show a smaller number (one each for a buffer's content/amount of steam).
+![](/Screenshots/Control%20Panel%20Manually%20Flushed.jpg)
+
+
+
+[^1]: [Refined Power Mod](https://ficsit.app/mod/RefinedPower)
+[^2]: [Circuitry Mod](https://ficsit.app/mod/FicsitWiremod)
---
+++ pressureRelief.lua
@@ -0,0 +1,198 @@
+/*
+Setup all the modules needed for the computer and control panel
+
+Each Fluid Container/Buffer has the following:
+- a valve on the input side
+- a 2 position switch
+- an indicator light
+- a micro LED screen to display how full it is
+- a guage to display the flow of steam into the buffer
+*/
+
+controlPanel = component.proxy("3AD3518A4058C87D99DE69B3A31A1065")
+computer = component.proxy("C1393A074F7CD4EB5CB26BAAD70C10B9")
+
+--text displays on the control panel
+screenL = controlPanel:getModule()
+screenR = controlPanel:getModule()
+
+--luid Buffer 1
+buffer1 = component.proxy("0542909A4C300E9013608885CB5A7009")
+bufferIndicator1 = controlPanel:getModule(0,5,0)
+buffer1LEDScreen = controlPanel:getModule(0,2,0)
+bufferSwitch1 = controlPanel:getModule(0,4,0)
+bufferFlowValve1 = component.proxy("6FE9C34F442BE616C508DB9B7B41D3CD")
+bufferFlowGauge1 = controlPanel:getModule(0,1,0)
+bufferFlowGauge1.limit = 1.0
+
+--Fluid Buffer 2
+buffer2 = component.proxy("3B065525406973657944079363AFD762")
+bufferIndicator2 = controlPanel:getModule(1,5,0)
+buffer2LEDScreen = controlPanel:getModule(1,2,0)
+bufferSwitch2 = controlPanel:getModule(1,4,0)
+bufferFlowValve2 = component.proxy("C9B30B244A2865ED142F8C96ACCCE68C")
+bufferFlowGauge2 = controlPanel:getModule(1,1,0)
+bufferFlowGauge2.limit = 1.0
+
+--Fluid Buffer 2
+buffer3 = component.proxy("83D308C14E6E1F689B9AA7AA9975A77B")
+bufferIndicator3 = controlPanel:getModule(2,5,0)
+buffer3LEDScreen = controlPanel:getModule(2,2,0)
+bufferSwitch3 = controlPanel:getModule(2,4,0)
+bufferFlowValve3 = component.proxy("B6C0E2AE4CDFBC7EC030FFB676CF8981")
+bufferFlowGauge3 = controlPanel:getModule(2,1,0)
+bufferFlowGauge3.limit = 1.0
+
+--lever switch & indicator
+manFlushSafety = controlPanel:getModule(3,4,0)
+manFlushIndicator = controlPanel:getModule(3,5,0)
+manFlushIndicator:setColor(0,0,0,0)
+manFlushLEDScreen = controlPanel:getModule(4,5,0)
+
+--the Valve for releasing the excess to more turbines/generators & a guage/LED screen combo on the control panel
+valveOverflow = component.proxy("48B15DC640BE87DA828CBB9B089BBAD5")
+valveLEDScreen = controlPanel:getModule(7,2,0)
+valveOverflowGauge = controlPanel:getModule(7,3,0)
+
+--a stop button to use for manually emptying the fluid buffers if needed
+flushBtn = controlPanel:getModule(4,3,0)
+
+--if any buffer is at or over 75% of total capacity then return true (so we can open the valve)
+function bufferCapacity()
+ local b1P = buffer1.fluidContent / buffer1.maxFluidContent
+ local b2P = buffer1.fluidContent / buffer2.maxFluidContent
+ local b3P = buffer1.fluidContent / buffer3.maxFluidContent
+
+ if (b1P or b2P or b3P) >= .75 --at or above 75% capacity in the buffers
+  then
+   return true
+  else
+   return false
+ end
+end
+
+--primary function used to let the system automatically open and close the valve; also allows for manually flushing the buffers
+function autoMode()
+ local flushing = true
+
+ if s == flushBtn and (buffer1Bool == true and buffer2Bool == true and buffer3Bool == true and manFlushSafety.state == true)
+ then --trigger the buffers to get flushed
+  buffer1:flush()
+  buffer2:flush()
+  buffer3:flush()
+ end
+   
+--update some UI on the control panel
+ if bufferCapacity()
+ then
+  flushing = true
+  manFlushLEDScreen:setText("Flushing...")
+  valveLEDScreen:setText("Open")
+  valveOverflow.userflowLimit = 150
+  valveOverflowGauge.percent = 1
+ else
+  flushing = false
+  valveLEDScreen:setText("Closed")
+  valveOverflowGauge.percent = 0
+  valveOverflow.userflowLimit = 0
+ end
+end
+
+--code that manipulates the control panel components
+local buffer1Bool = false
+local buffer2Bool = false
+local buffer3Bool = false
+local bufferFull = false
+
+while true do
+ buffer1LEDScreen:setText(math.ceil(buffer1.fluidContent))
+ buffer2LEDScreen:setText(math.ceil(buffer2.fluidContent))
+ buffer3LEDScreen:setText(math.ceil(buffer3.fluidContent))
+ bufferFlowGauge1.percent = bufferFlowValve1.flow
+ bufferFlowGauge2.percent = bufferFlowValve2.flow
+ bufferFlowGauge3.percent = bufferFlowValve3.flow
+ 
+
+ event.listen(bufferSwitch1, bufferSwitch2, bufferSwitch3, manFlushSafety, buffer1, buffer2, buffer3, flushBtn)
+ e, s = event.pull(0.0)
+ if s == bufferSwitch3 and bufferSwitch3.state == true
+ then
+  bufferSwitch3:setColor(1,1,1,2)
+  bufferIndicator3:setColor(1,0,0,1)
+  buffer3Bool = true
+ elseif s == bufferSwitch3 and bufferSwitch3.state == false
+ then
+  bufferSwitch3:setColor(0,0,0,1)
+  bufferIndicator3:setColor(1,0,0,0)
+  buffer3Bool = false
+ elseif s == bufferSwitch1 and bufferSwitch1.state == true
+ then
+  bufferSwitch1:setColor(1,1,1,2)
+  bufferIndicator1:setColor(1,0,0,1)
+  buffer1Bool = true
+ elseif s == bufferSwitch1 and bufferSwitch1.state == false
+ then
+  bufferSwitch1:setColor(0,0,0,1)
+  bufferIndicator1:setColor(1,0,0,0)
+  buffer1Bool = false
+ elseif s == bufferSwitch2 and bufferSwitch2.state == true
+ then
+  bufferSwitch2:setColor(1,1,1,2)
+  bufferIndicator2:setColor(1,0,0,1)
+  buffer2Bool = true
+ elseif s == bufferSwitch2 and bufferSwitch2.state == false
+ then
+  bufferSwitch2:setColor(0,0,0,1)
+  bufferIndicator2:setColor(1,0,0,0)
+  buffer2Bool = false
+ end
+ 
+ if buffer1Bool == true and buffer2Bool == true and buffer3Bool == true and manFlushSafety.state == false
+  then
+   manFlushIndicator:setColor(1,1,0,1)
+   manFlushLEDScreen:setText("Confirm...")
+  else
+   manFlushIndicator:setColor(1,1,0,0)
+ end
+ if buffer1Bool == true and buffer2Bool == true and buffer3Bool == true and manFlushSafety.state == true
+  then
+   bufferIndicator1:setColor(0,1,0,1)
+   bufferIndicator2:setColor(0,1,0,1)
+   bufferIndicator3:setColor(0,1,0,1)
+   manFlushIndicator:setColor(0,1,0,1)
+   manFlushLEDScreen:setText("Ready...")
+  else
+   manFlushIndicator:setColor(1,1,0,1)
+ end
+ if (buffer1Bool == false or buffer2Bool == false or buffer3Bool == false)
+ then
+   manFlushLEDScreen:setText("Auto Mode")
+   autoMode()
+  if buffer1Bool == true
+   then bufferIndicator1:setColor(1,0,0,1)
+   else
+    bufferIndicator1:setColor(1,0,0,0)
+    manFlushIndicator:setColor(1,1,0,0)
+  end
+  if buffer2Bool == true
+   then bufferIndicator2:setColor(1,0,0,1)
+   else
+    bufferIndicator2:setColor(1,0,0,0)
+    manFlushIndicator:setColor(1,1,0,0)
+  end
+  if buffer3Bool == true
+   then bufferIndicator3:setColor(1,0,0,1)
+   else
+    bufferIndicator3:setColor(1,0,0,0)
+    manFlushIndicator:setColor(1,1,0,0)
+  end
+ end
+
+--manually flush the buffers when the stop button is pressed
+ if s == flushBtn and (buffer1Bool == true and buffer2Bool == true and buffer3Bool == true and manFlushSafety.state == true)
+ then
+  buffer1:flush()
+  buffer2:flush()
+  buffer3:flush()
+ end
+end
